5b2f1ce7a6f30a163b324aa9a2908c01ef1ff27d
Zhu Weiwei <Weite@rich.richer.com>
2024-12-23 21:06:14 +0800

    增加：数据验证API

---
+++ apps/other/structs.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
--- apps/other/views.py
+++ apps/other/views.py
@@ -1,6 +1,10 @@
-from . import otherRouter
-
+from typing import Optional,List
+from fastapi import Query, Path
 import random
+
+
+from . import otherRouter
+from .structs import Item
 
 @otherRouter.get("/remove/")
 def add():
@@ -22,9 +26,49 @@ async def otherAPI(other):
     }
 
 
-@otherRouter.get("/random")
-def random(start: int = 0, end: int = 1000, base: int | None = None):
+@otherRouter.get("/random/")
+def random(start: int = 0, end: int = 1000, base: Optional[int]= None):
 
     return {
         "message": random.randint(start,end)
+    }
+
+@otherRouter.post("/item/")
+def item(item:Item):
+    
+    return item
+
+
+
+@otherRouter.get("/Verify phone number")
+def verify_phone_number(number: str = Query(length=11)):
+
+    return {
+        "message": "success",
+        "number":number
+    }
+
+@otherRouter.get("/Verify phone number/{number}/")
+def verify_phone_number(number: str = Path(length=11)):
+
+    return {
+        "message": "success",
+        "number":number
+    }
+
+@otherRouter.get("/Verify phone numbers")
+def verify_phone_number(numbers: List[str] = Query(length=11)):
+
+    return {
+        "message": "success",
+        "number":numbers
+    }
+
+
+@otherRouter.get("/Verify email")
+def verify_email(email: str = Query(pattern="^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$")):
+
+    return {
+        "message": "success",
+        "email":email
     }
